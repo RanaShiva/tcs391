7f5355039cdd682bc96b4ae574717d7a72069150
shiva-rana <shivam4learning@gmail.com>
2020-11-16 21:08:39 +0530

    2nd ques done

--- assignment1/2.cpp
+++ assignment1/2.cpp
@@ -1,1 +1,22 @@
 //Palindrome
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    cout<<"Enter the string: ";
+    char word[50];
+    cin.get(word, 50);
+    int i = strlen(word)-1;
+    for(int j=0; i>j; j++)
+    {
+        if(word[j] != word[i])
+        {
+            cout<<"Not palindrome\n";
+            return 0;
+        }  
+        i--;
+    }
+    cout<<"Palindrome\n";
+    return 0;
+}
